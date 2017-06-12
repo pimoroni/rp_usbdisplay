bafe0b33fa7928ed3222555e1fef8aeffa12669d
Phil Howard <phil@gadgetoid.com>
2017-06-12 16:00:01 +0000

    RP USB Disp DKMS package

---
+++ dkms/README.md
@@ -0,0 +1,12 @@
+# RPi USB Display
+## Dynamic Kernel Module Support
+
+This directory contains a .deb file, plus the source used to build it, for rp_usbdisplay DKMS support.
+
+Install with:
+
+```
+sudo apt install dkms raspberrypi-kernel-headers
+sudo dpkg -i rp-usbdisplay-dkms_1.0_all.deb
+```
+
---
+++ dkms/rp_usbdisplay-1.0/src/.rp_usbdisplay.o.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/rpusbdisp/rp_usbdisplay-1.0/src/rp_usbdisplay.o := ld -EL    -r -o /home/pi/rpusbdisp/rp_usbdisplay-1.0/src/rp_usbdisplay.o /home/pi/rpusbdisp/rp_usbdisplay-1.0/src/src/main.o /home/pi/rpusbdisp/rp_usbdisplay-1.0/src/src/usbhandlers.o /home/pi/rpusbdisp/rp_usbdisplay-1.0/src/src/fbhandlers.o /home/pi/rpusbdisp/rp_usbdisplay-1.0/src/src/touchhandlers.o 
